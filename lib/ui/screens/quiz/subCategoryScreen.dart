2f035e9a3e3c6962cd298b47d69b10c480d498e8
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-10-08 23:29:50 -0400

    Update subCaategoryScreen.dart

--- lib/ui/screens/quiz/subCategoryScreen.dart
+++ lib/ui/screens/quiz/subCategoryScreen.dart
@@ -7,10 +7,13 @@ import 'package:flutterquiz/app/routes.dart';
 import 'package:flutterquiz/features/profileManagement/cubits/userDetailsCubit.dart';
 import 'package:flutterquiz/features/quiz/cubits/subCategoryCubit.dart';
 import 'package:flutterquiz/features/quiz/models/quizType.dart';
+import 'package:flutterquiz/features/quiz/models/subcategory.dart';
 import 'package:flutterquiz/ui/widgets/bannerAdContainer.dart';
 import 'package:flutterquiz/ui/widgets/circularProgressContainer.dart';
 import 'package:flutterquiz/ui/widgets/customAppbar.dart';
 import 'package:flutterquiz/ui/widgets/errorContainer.dart';
+import 'package:flutterquiz/ui/widgets/premium_category_access_badge.dart';
+import 'package:flutterquiz/ui/widgets/unlock_premium_category_dialog.dart';
 import 'package:flutterquiz/utils/constants/error_message_keys.dart';
 import 'package:flutterquiz/utils/ui_utils.dart';
 
@@ -18,24 +21,27 @@ class SubCategoryScreen extends StatefulWidget {
   final String categoryId;
   final QuizTypes quizType;
   final String categoryName;
+  final bool isPremiumCategory;
 
   const SubCategoryScreen({
     super.key,
     required this.categoryId,
     required this.quizType,
     required this.categoryName,
+    required this.isPremiumCategory,
   });
 
   @override
   State<SubCategoryScreen> createState() => _SubCategoryScreenState();
 
   static Route<dynamic> route(RouteSettings routeSettings) {
-    Map arguments = routeSettings.arguments as Map;
+    final args = routeSettings.arguments as Map<String, dynamic>;
     return CupertinoPageRoute(
       builder: (_) => SubCategoryScreen(
-        categoryId: arguments['categoryId'],
-        quizType: arguments['quizType'],
-        categoryName: arguments['category_name'],
+        categoryId: args['categoryId'],
+        quizType: args['quizType'],
+        categoryName: args['category_name'],
+        isPremiumCategory: args['isPremiumCategory'] ?? false,
       ),
     );
   }
@@ -57,32 +63,47 @@ class _SubCategoryScreenState extends State<SubCategoryScreen> {
     getSubCategory();
   }
 
-  void handleListTileTap(String? subcategoryName, String? id, bool isPlayed) {
+  void handleListTileTap(Subcategory subCategory) {
+    /// Unlock Premium Subcategory and exit function
+    if (subCategory.isPremium && !subCategory.hasUnlocked) {
+      showUnlockPremiumCategoryDialog(
+        context,
+        categoryId: subCategory.mainCatId!,
+        subcategoryId: subCategory.id,
+        categoryName: subCategory.subcategoryName!,
+        requiredCoins: subCategory.requiredCoins,
+      );
+      return;
+    }
     if (widget.quizType == QuizTypes.guessTheWord) {
       Navigator.of(context).pushNamed(Routes.guessTheWord, arguments: {
         "type": "subcategory",
-        "typeId": id,
-        "isPlayed": isPlayed,
+        "typeId": subCategory.id,
+        "isPlayed": subCategory.isPlayed,
+        "isPremiumCategory": subCategory.isPremium || widget.isPremiumCategory,
       });
     } else if (widget.quizType == QuizTypes.funAndLearn) {
       Navigator.of(context).pushNamed(Routes.funAndLearnTitle, arguments: {
         "type": "subcategory",
-        "typeId": id,
-        "title": subcategoryName,
+        "typeId": subCategory.id,
+        "title": subCategory.subcategoryName,
+        "isPremiumCategory": subCategory.isPremium || widget.isPremiumCategory,
       });
     } else if (widget.quizType == QuizTypes.audioQuestions) {
       Navigator.of(context).pushNamed(Routes.quiz, arguments: {
         "numberOfPlayer": 1,
         "quizType": QuizTypes.audioQuestions,
-        "subcategoryId": id,
-        "isPlayed": isPlayed,
+        "subcategoryId": subCategory.id,
+        "isPlayed": subCategory.isPlayed,
+        "isPremiumCategory": subCategory.isPremium || widget.isPremiumCategory,
       });
     } else if (widget.quizType == QuizTypes.mathMania) {
       Navigator.of(context).pushNamed(Routes.quiz, arguments: {
         "numberOfPlayer": 1,
         "quizType": QuizTypes.mathMania,
-        "subcategoryId": id,
-        "isPlayed": isPlayed,
+        "subcategoryId": subCategory.id,
+        "isPlayed": subCategory.isPlayed,
+        "isPremiumCategory": subCategory.isPremium || widget.isPremiumCategory,
       });
     }
   }
@@ -116,7 +137,7 @@ class _SubCategoryScreenState extends State<SubCategoryScreen> {
           );
         }
 
-        final subCategoryList =
+        final subcategories =
             (state as SubCategoryFetchSuccess).subcategoryList;
         return ListView.separated(
           padding: EdgeInsets.symmetric(
@@ -124,19 +145,14 @@ class _SubCategoryScreenState extends State<SubCategoryScreen> {
             horizontal: MediaQuery.of(context).size.width * UiUtils.hzMarginPct,
           ),
           shrinkWrap: true,
-          itemCount: subCategoryList.length,
+          itemCount: subcategories.length,
           physics: const AlwaysScrollableScrollPhysics(),
           separatorBuilder: (_, i) =>
               const SizedBox(height: UiUtils.listTileGap),
           itemBuilder: (BuildContext context, int index) {
+            final subcategory = subcategories[index];
             return GestureDetector(
-              onTap: () {
-                handleListTileTap(
-                  subCategoryList[index].subcategoryName,
-                  subCategoryList[index].id,
-                  subCategoryList[index].isPlayed,
-                );
-              },
+              onTap: () => handleListTileTap(subcategory),
               child: LayoutBuilder(
                 builder: (context, boxConstraints) {
                   return Stack(
@@ -197,7 +213,7 @@ class _SubCategoryScreenState extends State<SubCategoryScreen> {
                                     memCacheWidth: 50,
                                     memCacheHeight: 50,
                                     placeholder: (_, __) => const SizedBox(),
-                                    imageUrl: subCategoryList[index].image!,
+                                    imageUrl: subcategory.image!,
                                     errorWidget: (_, i, e) => Image(
                                       image: AssetImage(
                                         UiUtils.getImagePath("ic_launcher.png"),
@@ -214,7 +230,7 @@ class _SubCategoryScreenState extends State<SubCategoryScreen> {
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
                                     Text(
-                                      subCategoryList[index].subcategoryName!,
+                                      subcategory.subcategoryName!,
                                       maxLines: 1,
                                       style: TextStyle(
                                         color: Theme.of(context)
@@ -227,7 +243,7 @@ class _SubCategoryScreenState extends State<SubCategoryScreen> {
                                     if (widget.quizType ==
                                         QuizTypes.funAndLearn) ...[
                                       Text(
-                                        "comprehensive: ${subCategoryList[index].noOfQue!}",
+                                        "comprehensive: ${subcategory.noOfQue!}",
                                         style: TextStyle(
                                           fontSize: 14,
                                           color: Theme.of(context)
@@ -238,7 +254,7 @@ class _SubCategoryScreenState extends State<SubCategoryScreen> {
                                       ),
                                     ] else ...[
                                       Text(
-                                        "Question: ${subCategoryList[index].noOfQue!}",
+                                        "Question: ${subcategory.noOfQue!}",
                                         style: TextStyle(
                                           fontSize: 14,
                                           color: Theme.of(context)
@@ -254,21 +270,32 @@ class _SubCategoryScreenState extends State<SubCategoryScreen> {
                               const SizedBox(width: 10),
 
                               /// right arrow
-                              Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(5.0),
-                                  border: Border.all(
+                              Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  PremiumCategoryAccessBadge(
+                                    hasUnlocked: subcategory.hasUnlocked,
+                                    isPremium: subcategory.isPremium,
+                                  ),
+                                  const SizedBox(width: 10),
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(5.0),
+                                      border: Border.all(
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .onTertiary
+                                              .withOpacity(0.1)),
+                                    ),
+                                    child: Icon(
+                                      Icons.keyboard_arrow_right_rounded,
+                                      size: 30,
                                       color: Theme.of(context)
                                           .colorScheme
-                                          .onTertiary
-                                          .withOpacity(0.1)),
-                                ),
-                                child: Icon(
-                                  Icons.keyboard_arrow_right_rounded,
-                                  size: 30,
-                                  color:
-                                      Theme.of(context).colorScheme.onTertiary,
-                                ),
+                                          .onTertiary,
+                                    ),
+                                  ),
+                                ],
                               ),
                             ],
                           ),
